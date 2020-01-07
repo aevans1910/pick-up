1e6e1cc0b0c865b2f3efd5e9c66094ea74079613
Ariane Evans <ariane.evans@student.makeschool.com>
2020-01-07 15:30:29 -0800

    [progress] on flask templating and page changing

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/location')
+def location():
+    return render_template('location.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/rewards')
+def rewards():
+    return render_template('rewards.html')
